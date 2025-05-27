bc7b1d041ab1d1095a816a01d99c528744da5e0c
bensonc444 <bensonchenxd444@gmail.com>
2025-05-26 23:18:51 -0400

    PartB.java

--- Q1/PartB.java
+++ Q1/PartB.java
@@ -1,1 +1,12 @@
-
+public int dogWalkShift(int startHour, int endHour)
+{
+  int earnings = 0;
+  for (int i= startHours;i<=endHours;i++){
+    int dogsWalked=walkDogs(i);
+    earnings +=dogsWalked*5;
+    if (if(hour >= 9 && hour <= 17) || dogsWalked == maxDogs){
+      earnings +=3;
+    }
+  }
+  return earnings;
+}
